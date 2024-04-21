9fd3548d12cc694918bb6a9f2928a1db8e9bd636
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-21 19:08:42 +0530

    Added fixes to validate user inputs

--- Projects/simple_calculator/calc.py
+++ Projects/simple_calculator/calc.py
@@ -72,27 +72,78 @@ def mod(number1, number2):
     """
     return number1 % number2
 
+def print_usage():
+    """This program prints usage
+    """
+    print("""Usage: python calc.py <action> <number1>  <number2>
+          action:  add | sub | mul | div | mod
+          number1 is integer
+          number2 is integer
+          """)
 
-# get arguments excluding file name
-arguments = sys.argv[1::]
-# get action
-action = arguments[0]
-# get number 1
-number1 = int(arguments[1])
-# get number 2
-number2 = int(arguments[2])
+def is_int(value):
+    try:
+        int(value)
+    except ValueError:
+        print()
+        return False
+    return True
 
-if action.lower() == 'add':
-    result = add(number1, number2)
-elif action.lower() == 'sub':
-    result = sub(number1, number2)
-elif action.lower() == 'mul':
-    result = mul(number1, number2)
-elif action.lower() == 'div':
-    result = div(number1, number2)
-elif action.lower() == 'mod':
-    result = mod(number1, number2)
-else:
-    print("wrong operation provided")
-    sys.exit(1)
-print(result)
+def validate_arguments(arguments):
+    """This function validates arguments and displays error 
+    message to the user
+
+    Arguments: arguments
+
+    Returns:
+      True if the arguments are valid False otherwise
+    """
+    if len(arguments) != 3:
+        print_usage()
+        return False
+    valid_actions = ('add', 'sub', 'mul', 'div', 'mod')
+    if arguments[0].lower() not in valid_actions:
+        print("Invalid action")
+        print_usage()
+        return False    
+    #todo: We still need to validate if the values are 
+    # integers or not
+    if not is_int(arguments[1]) or  not is_int(arguments[2]):
+        print("pass integers only")
+        print_usage()
+        return False
+    return True
+
+
+def main(arguments):
+    """This function executes when the file is called directly
+
+    Arguments:
+       arguments
+    """
+    if not validate_arguments(arguments):
+        sys.exit(1)
+    # get action
+    action = arguments[0]
+    # get number 1
+    number1 = int(arguments[1])
+    # get number 2
+    number2 = int(arguments[2])
+
+    if action.lower() == 'add':
+        result = add(number1, number2)
+    elif action.lower() == 'sub':
+        result = sub(number1, number2)
+    elif action.lower() == 'mul':
+        result = mul(number1, number2)
+    elif action.lower() == 'div':
+        result = div(number1, number2)
+    elif action.lower() == 'mod':
+        result = mod(number1, number2)
+    else:
+        print("wrong operation provided")
+        sys.exit(1)
+    print(result)
+
+if __name__ == "__main__":
+    main(sys.argv[1::])
