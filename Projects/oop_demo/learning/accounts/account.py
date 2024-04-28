ed34e480e119ac2a270ed0cfe534a01a06342dad
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-28 10:15:50 +0530

    Added pytests

--- Projects/oop_demo/learning/accounts/account.py
+++ Projects/oop_demo/learning/accounts/account.py
@@ -1,11 +1,15 @@
 """This module will have Bank Account
 """
 
+
 class BankAccount:
     """
     This class will have basic Bank Account Functionality
     """
-    def __init__(self, account_number:str,name:str, initial_deposit:float = 0) -> None:
+
+    def __init__(
+        self, account_number: str, name: str, initial_deposit: float = 0
+    ) -> None:
         """
         This method initializes the object with initial values
         """
@@ -24,3 +28,46 @@ class BankAccount:
         This method deposits amount
         """
         self.balance += amount
+
+    def account_type(self):
+        """
+        This method will return the account_type
+
+        Returns:
+          "Savings" for Savings Account
+          "Current" for Current Account
+          None for others
+        """
+        if isinstance(self,SavingsAccount):
+            return "Savings"
+        elif isinstance(self, CurrentAccount):
+            return "Current"
+        return None
+
+
+class SavingsAccount(BankAccount):
+    """This class represents the Savings Account Structure"""
+
+    def withdraw(self, amount):
+        """
+        This method is overriden to implement non negative balances
+        """
+        if self.balance - amount < 0:
+            # dont allow this transaction
+            # fail this
+            print("This transaction is not allowed")
+            return
+        return super().withdraw(amount)
+
+    def deposit(self, amount):
+        """
+        This method stops the user to deposit more than 10 lakhs
+        """
+        if amount > 1000000:
+            print("Not allowed")
+            return
+        return super().deposit(amount)
+
+
+class CurrentAccount(BankAccount):
+    """This represents the Current Account"""
--- Projects/oop_demo/tests/accounts/test_bankaccount.py
+++ Projects/oop_demo/tests/accounts/test_bankaccount.py
@@ -1,19 +1,19 @@
 """
 This test cases will be arround bank account
 """
-from learning.accounts.account import BankAccount
+
+from learning.accounts.account import BankAccount, SavingsAccount, CurrentAccount
+import pytest
+
 
 def test_bankaccount_basics():
     """
-    This method checks the 
+    This method checks the
         account creation
-        depositing the amount 
+        depositing the amount
         withdrawing the amount
     """
-    account_my = BankAccount(
-        account_number="x12345",
-        name="LT",
-        initial_deposit=10000)
+    account_my = BankAccount(account_number="x12345", name="LT", initial_deposit=10000)
     # checking if the balance is same as initial deposit
     assert account_my.balance == 10000
     # deposit 1000 more and check blance
@@ -23,3 +23,62 @@ def test_bankaccount_basics():
     # withdraw 5000 and check balance
     account_my.withdraw(5000)
     assert account_my.balance == 6000
+
+
+def test_savings_account():
+    """
+    This method will test savings account
+    """
+    my_savings_account = SavingsAccount(
+        account_number="x12345", name="LT", initial_deposit=5000
+    )
+    # to be fixed
+    assert my_savings_account.account_type() == "Savings"
+    assert my_savings_account.balance == 5000
+    # try withdrawing more than account balance,
+    # the balance should not change
+    # todo: This operation should result in error
+    my_savings_account.withdraw(6000)
+
+    assert my_savings_account.balance == 5000
+
+    my_savings_account.deposit(1000)
+    assert my_savings_account.balance == 6000
+    # now withdraw valid balance,it should be allowed
+    my_savings_account.withdraw(6000)
+    assert my_savings_account.balance == 0
+    # try depositing more than 10 lakhs and that should not be allowed
+    my_savings_account.deposit(1100000)
+    assert my_savings_account.balance == 0
+
+
+@pytest.mark.parametrize(
+    "initial_deposit,account_number,name",
+    [
+        (10000, "x1234", "LT"),
+        (20000, "x1234", "LT"),
+        (30000, "x1234", "LT"),
+        (40000, "x1234", "LT"),
+        (50000, "x1234", "LT"),
+    ],
+)
+def test_current_account(initial_deposit, account_number, name):
+    """
+    Testing current account
+    """
+    my_current_account = CurrentAccount(
+        account_number=account_number, name=name, initial_deposit=initial_deposit
+    )
+    # check for the type
+    assert my_current_account.account_type() == "Current"
+    # withdraw more than deposited amount
+    current_balance = my_current_account.balance
+
+    my_current_account.withdraw(current_balance + 10000)
+
+    assert my_current_account.balance == -10000
+
+    # try depositing 50lakhs
+    amount = 5000000
+    my_current_account.deposit(amount)
+    assert my_current_account.balance == amount - 10000
