8dfb00f8c7a142dc656b29775d8b634227dcbbdc
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-05-11 18:59:26 +0530

    Added changes

--- cloud/aws/ec2utils.py
+++ cloud/aws/ec2utils.py
@@ -17,6 +17,37 @@ def stop_ec2(instance_id):
     return response
 
 
+def ec2_instance_ids(tag_name, tag_value):
+    """This method will return a list of instance ids 
+    based on tag name and value
+   
+    Arguments:
+      tag_name: str
+      tag_value: str
+
+    Returns:
+      list
+    """
+    instance_ids = []
+    client = boto3.client('ec2')
+    response = client.describe_instances(
+        Filters = [
+            {
+                'Name': f"tag:{tag_name}",
+                'Values': [tag_value]
+            }
+        ]
+    )
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_ids.append(instance['InstanceId'])
+    return instance_ids
+
+
+
+
 if __name__ == '__main__':
-    instance_id = input("Enter your ec2 instance id: ")
-    start_ec2(instance_id)
+    instance_ids = ec2_instance_ids('Name', 'sample')
+    for instance_id in instance_ids:
+        stop_ec2(instance_id)
+
